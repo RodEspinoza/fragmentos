d8551e5a205ae211db075d40233b72465653c5b1
novacane <espinoza.sample@gmail.com>
2018-10-12 23:07:33 -0300

    Falta terminar crud editar

--- app/src/main/java/com/example/rodrigoespinoza/fragmentos/fragments/EditProductFragment.java
+++ app/src/main/java/com/example/rodrigoespinoza/fragmentos/fragments/EditProductFragment.java
@@ -7,8 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.rodrigoespinoza.fragmentos.R;
+import com.example.rodrigoespinoza.fragmentos.model.Product;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,35 +22,31 @@ import com.example.rodrigoespinoza.fragmentos.R;
  * create an instance of this fragment.
  */
 public class EditProductFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Button btnUpdateProduct, btnDeleteProduct;
+    EditText txtFragEditProductStock, txFragEditProductName;
+    Product product;
+    View view;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     private OnFragmentInteractionListener mListener;
 
     public EditProductFragment() {
-        // Required empty public constructor
+
     }
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+
      * @return A new instance of fragment EditProductFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static EditProductFragment newInstance(String param1, String param2) {
         EditProductFragment fragment = new EditProductFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,17 +54,19 @@ public class EditProductFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_product, container, false);
+        this.view = inflater.inflate(R.layout.fragment_edit_product, container, false);
+        this.btnUpdateProduct = this.view.findViewById(R.id.btnFragEditProduct);
+        this.btnDeleteProduct = this.view.findViewById(R.id.btnFragDeleteProduct);
+        this.txFragEditProductName = this.view.findViewById(R.id.txFragEditProductName);
+        this.txtFragEditProductStock = this.view.findViewById(R.id.txtFragEditProductStock);
+        //this.btnDeleteProduct.setOnClickListener();
+        return this.view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
