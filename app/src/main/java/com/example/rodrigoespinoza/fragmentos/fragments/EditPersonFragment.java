b798c651e92ac578e7355c72f9b10b5e41b3de19
Enzo Soto <enz.soto@gmail.com>
2018-10-14 16:03:30 -0300

    commit

--- app/src/main/java/com/example/rodrigoespinoza/fragmentos/fragments/EditPersonFragment.java
+++ app/src/main/java/com/example/rodrigoespinoza/fragmentos/fragments/EditPersonFragment.java
@@ -1,5 +1,6 @@
 package com.example.rodrigoespinoza.fragmentos.fragments;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -46,7 +47,7 @@ public class EditPersonFragment extends Fragment {
     Button btnFragEditPersonEdit;
 
     Integer idUser;
-    String localidad;
+    String localidad, sexo;
 
     private OnFragmentInteractionListener mListener;
 
@@ -83,16 +84,20 @@ public class EditPersonFragment extends Fragment {
         this.txtFragEditPersonName = (EditText) this.view.findViewById(R.id.txtFragEditPersonName);
         this.txtFragEditPersonLastName = (EditText) this.view.findViewById(R.id.txtFragEditPersonLastName);
         this.rgFragEditPerson = (RadioGroup) this.view.findViewById(R.id.rgFragEditPerson);
+        this.rgFragEditPerson.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.rbFragRegistroHombre){
+                    sexo = "Masculino";
+                } else{
+                    sexo = "Femenino";
+                }
+            }
+        });
+
         this.rbFragEditPersonMasculino = (RadioButton) this.view.findViewById(R.id.rbFragEditPersonMasculino);
         this.rbFragEditPersonFemenino = (RadioButton) this.view.findViewById(R.id.rbFragEditPersonFemenino);
         this.spFragEditPersonLocalidad = (Spinner) this.view.findViewById(R.id.spFragEditPersonLocalidad);
-
-        Bundle menuBundle = this.getArguments();
-        if (menuBundle != null) {
-            idUser = Integer.parseInt(menuBundle.get("idUser").toString());
-        }
-        //obtenemos los valores guardados
-        getCampos(idUser);
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(getContext(), R.layout.support_simple_spinner_dropdown_item,
                 Utils.getLocations());
@@ -103,22 +108,59 @@ public class EditPersonFragment extends Fragment {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 localidad = spFragEditPersonLocalidad.getSelectedItem().toString();
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
 
+        Bundle menuBundle = this.getArguments();
+        if (menuBundle != null) {
+            idUser = Integer.parseInt(menuBundle.get("idUser").toString());
+        }
+        //obtenemos los valores guardados
+        getCampos(idUser);
+
         this.btnFragEditPersonEdit = (Button) this.view.findViewById(R.id.btnFragEditPersonEdit);
+        this.btnFragEditPersonEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actualizarPerson(txtFragEditPersonName.getText().toString(), txtFragEditPersonLastName.getText().toString(),
+                        sexo, localidad, idUser);
+            }
+        });
         return this.view;
     }
 
-    private void getCampos(Integer idPerson) {
+    private void actualizarPerson(String name, String last_name, String sexo, String localidad, Integer idUser) {
+        conn = new SqlConecttion(getContext(), "bd_gestor_pedidos", null, 1);
+        SQLiteDatabase db = conn.getWritableDatabase();
+
+        try {
+            String[] buscar = {idUser.toString()};
+            ContentValues values = new ContentValues();
+            values.put("name", name);
+            values.put("last_name", last_name);
+            values.put("sexo", sexo);
+            values.put("location", localidad);
+
+            db.update("person", values, "id = ?", buscar);
+
+            Toast.makeText(getContext(), "Actualizado", Toast.LENGTH_SHORT).show();
+            conn.close();
+            db.close();
+        } catch (Exception ex){
+            Toast.makeText(getContext(),"Error a actualizar", Toast.LENGTH_SHORT).show();
+            conn.close();
+            db.close();
+        }
+    }
+
+    private void getCampos(Integer idUser) {
         conn = new SqlConecttion(getContext(), "bd_gestor_pedidos", null, 1);
         SQLiteDatabase db = conn.getReadableDatabase();
 
-        String[] buscar = {idPerson.toString()};
+        String[] buscar = {idUser.toString()};
         //String[] obtener = {"name", "last_name", "sexo"};
 
         Cursor cursor = db.rawQuery("SELECT * FROM person WHERE id = ?", buscar);
