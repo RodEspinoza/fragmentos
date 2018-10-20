23e7e78653efe8dddff12199065afbd87903cdc0
novacane <espinoza.sample@gmail.com>
2018-10-20 02:30:42 -0300

    nani

---
+++ wsAndroid/wsGetAllProducts.php
@@ -0,0 +1,32 @@
+<?php
+
+$hostname_localhost = "localhost";
+$database_localhost = "id3415554_bd_gestor_pedidos";
+$username_locahost = "id3415554_root";
+$password_localhost = "siempretropical";
+
+$json=array();
+
+
+
+	$conexion = mysqli_connect($hostname_localhost,$username_locahost,$password_localhost,$database_localhost);
+  if(mysqli_connect_errno){
+      echo "".mysqli_connect_error();
+     }
+  $consulta = "SELECT * FROM producto";
+
+
+	$resultado=mysqli_query($conexion,$consulta);
+
+	while($registro=mysqli_fetch_array($resultado)){
+		$json['usuario'][] = $registro;
+	}
+
+
+	mysqli_close($conexion);
+	echo json_encode($json);
+
+
+
+
+?>
