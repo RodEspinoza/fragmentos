22ffdb923e06eaed4e5e5ca48f7248fd983dfb20
Enzo Soto <enz.soto@gmail.com>
2018-10-20 17:17:26 -0300

    commit

--- wsAndroid/wsIngresarUsuario.php
+++ wsAndroid/wsIngresarUsuario.php
@@ -1,49 +1,40 @@
 <?php
 
 $hostname_localhost = "localhost";
-$database_localhost = "bd_usuarios";
-$username_locahost = "root";
-$password_localhost = "";
+$database_localhost = "u114296893_gesto";
+$username_locahost = "u114296893_root";
+$password_localhost = "siempretropical";
 
 $json=array();
 
+if(isset($_POST["email"]) && isset($_POST["pass"]) && isset($_POST["fecha"])) {
 
-if(isset($_GET["nombre"])&&isset($_GET["apellido"])&&isset($_GET["rut"])){
+	$email = $_POST["email"];
+	$pass = $_POST["pass"];
+	$fecha = $_POST["fecha"];
 
-	$nombre=$_GET["nombre"];
-	$apellido=$_GET["apellido"];
-	$rut=$_GET["rut"];
+	$conexion = mysqli_connect($hostname_localhost, $username_locahost, $password_localhost, $database_localhost);
 
-	$conexion = mysqli_connect($hostname_localhost,$username_locahost,$password_localhost,$database_localhost);
-	$insert = "INSERT INTO usuario(nombre,apellido,rut) VALUES('{$nombre}','{$apellido}','{$rut}')";
+	$insert = "INSERT INTO usuario(email,pass,fecha) VALUES('{$email}','{$pass}','{$fecha}')";
+
 	$resultado_insert=mysqli_query($conexion,$insert);
-
-	$last_id = mysqli_insert_id($conexion);
-
-	if($resultado_insert){
-
-		$consulta = "SELECT * FROM usuario where id={$last_id}";
-		$resultado = mysqli_query($conexion,$consulta);
-
-		if($registro=mysqli_fetch_array($resultado)){
-			$json['usuario'][] = $registro;
-
-		}
-		mysqli_close($conexion);
-		echo json_encode($json);
-
+	
+	if(!$resultado_insert){
+		echo "".mysqli_error($conexion);
+	}else{
+	 $last_id = mysqli_insert_id($conexion);
+	 echo $last_id;
+	  $consulta = "SELECT * FROM PRODUCT WHERE id={$last_id}";
+	  $resultado = mysqli_query($conexion, $consulta);
+	  echo $resultado;
+	  if($registro = mysqli_fetch_array($resultado)){
+		  $json["usuario"] =$registro;
+	  }
+	  echo json_encode($json);
 
 	}
-	else{
-		$result["rut"]=0;
-		$json['usuario'][] = $result;
-		echo json_encode($json);
-	}
 
-}
-
-
-else{
+} else {
 
 }
 
