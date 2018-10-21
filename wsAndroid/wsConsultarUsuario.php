62bc3b29150919dbf41261e2318addcf584f73d9
Enzo Soto <enz.soto@gmail.com>
2018-10-21 15:37:38 -0300

    commit

--- wsAndroid/wsConsultarUsuario.php
+++ wsAndroid/wsConsultarUsuario.php
@@ -8,39 +8,27 @@ $password_localhost = "siempretropical";
 $json=array();
 
 
-if(isset($_POST["email"])&&isset($_POST["pass"])&&isset($_POST["fecha"])){
+if(isset($_POST["email"])&&isset($_POST["pass"])){
 
 	$email = $_POST["email"];
 	$pass = $_POST["pass"];
-	$fecha = $_POST["fecha"];
 
 	$conexion = mysqli_connect($hostname_localhost,$username_locahost,$password_localhost,$database_localhost);
-	$insert = "INSERT INTO usuario(email,pass,fecha) VALUES('{$email}','{$pass}','{$fecha}')";
-	$resultado_insert = mysqli_query($conexion,$insert);
+	$select = "SELECT id FROM usuario WHERE email = {$email} AND pass = {$pass}";
+	$resultado_select = mysqli_query($conexion,$insert);
 
-	$last_id = mysqli_insert_id($conexion);
+	if ($resultado_select) {
 
-	if($resultado_insert){
-
-		$consulta = "SELECT * FROM usuario where id={$last_id}";
-		$resultado = mysqli_query($conexion,$consulta);
-
-		if($registro = mysqli_fetch_array($resultado)){
-			$json['usuario'][] = $registro;
-		}
+		$json['id_usuario'][] = $resultado_select;
+		
 		mysqli_close($conexion);
 		echo json_encode($json);
 	}
 	else{
-		$result["id"]=0;
-		$json['usuario'][] = $result;
-		echo json_encode($json);
+		echo "Error: " . mysqli_error($conexion);
 	}
 
-}
-
-
-else{
+} else {
 
 }
 
