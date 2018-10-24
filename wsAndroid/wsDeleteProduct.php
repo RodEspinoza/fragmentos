882a5d97ccd215a735b177d4a6060ddbab31025d
novacane <espinoza.sample@gmail.com>
2018-10-23 23:41:48 -0300

    add ws delete product

---
+++ wsAndroid/wsDeleteProduct.php
@@ -0,0 +1,19 @@
+<?php
+
+$hostname_localhost = "localhost";
+$database_localhost = "u114296893_gesto";
+$username_locahost = "u114296893_root";
+$password_localhost = "siempretropical";
+$json=array();
+
+
+if(isset($_POST["rut"])){
+	$id=$_POST["id"];
+  $conexion = mysqli_connect($hostname_localhost,$username_locahost,$password_localhost,$database_localhost);
+  $stmt = $conexion->prepare("DELETE from producto WHERE id = '{id}'");
+	$stmt->execute();
+
+
+}
+
+?>
